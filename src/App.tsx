a8729f27972d498d3a0535d7bd7bc7c6a74914b1
zyper16 <zyper16@gmail.com>
2024-11-02 14:40:43 +0200

    Added routing & created main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
 function App() {
-  return <Container>Hi</Container>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      Hi
+    </Container>
+  );
 }
 
 export default App;
