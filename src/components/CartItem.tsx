bcd9d24cd60ddf6f15776eca5d2735892df7e9ff
zyper16 <zyper16@gmail.com>
2024-11-08 16:40:20 +0200

    Finished CartItem Component

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,3 +1,45 @@
-export default function CartItem() {
-  return <p>hi</p>;
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import storeItems from "../data/items.json";
+import formatPrice from "../utilities/formatPrice";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+export default function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find(i => i.id === id);
+  if (item == null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatPrice(item.price)}
+        </div>
+      </div>
+      <div> {formatPrice(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
+    </Stack>
+  );
 }
